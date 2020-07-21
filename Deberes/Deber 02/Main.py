70ffaac47976e90c9234702567bcf0cae704cf1c
Carlos Chicaiza <carlos.chicaiza02@epn.edu.ec>
2020-07-20 23:08:12 -0500

    deber finished

--- Deberes/Deber 02/Main.py
+++ Deberes/Deber 02/Main.py
@@ -1,22 +1,77 @@
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
 import random
+
+def generarBloques(n,ancho,alto):
+    blocks = []
+    
+    bloque_size = ancho/int(n)
+    bloque_size= int(bloque_size)
+    
+    for y in range(0, alto, bloque_size):
+        for x in range(0, ancho, bloque_size):
+            blocks.append(img_array[y:y+bloque_size, x:x+bloque_size])
+            
+    return blocks
+
+
+def graficarMatrix(n,blocks,num_pieza_sacada,lista):
+    aux = 0
+    n = int(n)
+    f, axarr = plt.subplots(n,n)
+    for x in range(n):
+        for y in range(n):
+            if(lista[aux] != num_pieza_sacada):
+                axarr[x,y].imshow(blocks[lista[aux]])        
+                axarr[x,y].axis('off')
+            aux= aux+1
+    plt.axis('off')
+    plt.show()
+
+def tranformarMatrixList(n, matrix):
+    lista =[]
+    n = int(n)
+    for j in range(n):
+        for i in matrix[j]:
+            lista.append(i)
+    return lista
+            
+#img = Image.open('helmet.jpg')
+img = Image.open('heinseberg.jpg')
+ancho = 600
+alto = 600
+new_img = img.resize((ancho,alto))
+
+img_array = np.array(new_img)
+
+
+plt.imshow(img_array, interpolation='nearest')
+plt.show()
+        
+        
 pieza=()
 movimientos = 0
 pieza_sacada=()
 lista_4x4 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 lista_3x3 = [0,1,2,3,4,5,6,7,8]
+lista_aux=[]
 
 while True:
     lista_escogida_numero = input("Escoja: Matrix de 4x4 (4) o de 3x3 (3)")
     if(lista_escogida_numero == '3'):
-        lista_escogida= lista_3x3            
+        lista_escogida= lista_3x3           
+        lista_aux = lista_3x3           
         break;
     elif(lista_escogida_numero == '4'):
         lista_escogida= lista_4x4
+        lista_aux = lista_4x4
         break;
     else:
         print("Escoja 4 o 3 para la dimensión de la matriz")
 random.shuffle(lista_escogida)
 print('\n'*2)
+
 
 matrix=[]
 while lista_escogida !=[]:
@@ -41,7 +96,12 @@ while True:
             print('\n+----+----+---|')        
         else:
             print('\n+----+----+----+---|') 
-    #ask for the next move
+
+    bloques =  generarBloques(lista_escogida_numero,ancho,alto)
+    
+    lista_cambiada = tranformarMatrixList(lista_escogida_numero, matrix)
+    graficarMatrix(lista_escogida_numero, bloques, num_pieza_sacada, lista_cambiada)
+    
     num = input('\nplease type the number of the piece to move : ( q ) to quit  ')
     if num in ['q','Q']:
         break
@@ -63,8 +123,10 @@ while True:
             pieza_sacada=(pieza[0],pieza[1])
             movimientos = movimientos +1
             print()
-            print('you have made ',movimientos , 'moves so far ')
+            print('Movimientos realizados ',movimientos)
             print(2*'\n')
         else:
-            print('illegal move , try again ')
-print('game over  ')
+            print('Movimiento invalido')
+print('Game Over')
+
+
