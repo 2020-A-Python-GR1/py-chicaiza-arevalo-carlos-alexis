25526e133760deb8bf28da6d7c8b90062e54d515
Carlos Chicaiza <carlos.chicaiza02@epn.edu.ec>
2020-09-01 07:29:06 -0500

    "deber"

--- 04 - scrapy/03-intro-spider/arania_basica/arania_basica/spiders/arania_ejemplo.py
+++ 04 - scrapy/03-intro-spider/arania_basica/arania_basica/spiders/arania_ejemplo.py
@@ -18,5 +18,31 @@ class IntroSpider(scrapy.Spider):
         titulos = etiqueta_contenedora.css(
             'h3 > a::text'
         ).extract()
+        imagenes = response.xpath(
+            '//a/img[@src]'
+        )
+        dineros = response.xpath(
+            '//li/article/div/p[@class ="price_color"]'
+        )
+        stocks = response.xpath(
+            '//li/article/div/p[@class ="instock availability"]'
+        )
+        estrellas = response.xpath(
+            '//section/div/ol/li/article/p'
+        )
+        #print(imagenes)1
+        print('imagenes')
+        for img in imagenes:
+            print (img)
+        
+        print('precios')
+        for precio in dineros:
+            print(precio)
 
-        print(titulos)
+        print('Stock')
+        for stock in stocks:
+            print(stock)
+        print('Estrellas')
+        for estrella in estrellas:
+            print(estrella)
+
